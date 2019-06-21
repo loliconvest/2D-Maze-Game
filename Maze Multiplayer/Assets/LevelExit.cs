acaf4e05047fdb8edba912ba1a8a38ce7afc974c
loliconvest <44865564+loliconvest@users.noreply.github.com>
2019-06-21 10:00:51 +0800

    Add empty project

---
+++ Maze Multiplayer/Assets/LevelExit.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    [SerializeField] float LevelLoadDelay = 1f;
+    // Start is called before the first frame update
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        StartCoroutine(LoadNextLevel());
+    }
+
+    // Update is called once per frame
+    IEnumerator LoadNextLevel()
+    {
+        yield return new WaitForSecondsRealtime(LevelLoadDelay);
+        
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
